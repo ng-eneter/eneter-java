32f00be42ccaaf31de6a1c0a28027c29d56444e3
Ondrej Uzovic <ondrej.uzovic@gmail.com>
2011-10-30 10:13:59 +0000

    Unit test for the event added.

---
+++ MessagingFrameworkTest/src/eneter/net/system/ClassWithEvent.java
@@ -0,0 +1,18 @@
+package eneter.net.system;
+
+public class ClassWithEvent
+{
+    public Event<String> nameUpdated()
+    {
+        return myNameUpdatedEvent;
+    }
+    
+    public void SetName(String name)
+    {
+        // Rise the event that the name was updated.
+        myNameUpdatedEventImpl.update(this, name);
+    }
+    
+    private EventImpl<String> myNameUpdatedEventImpl = new EventImpl<String>();
+    private Event<String> myNameUpdatedEvent = new Event<String>(myNameUpdatedEventImpl);
+}
---
+++ MessagingFrameworkTest/src/eneter/net/system/EventUTest.java
@@ -0,0 +1,59 @@
+package eneter.net.system;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class EventUTest
+{
+    /*
+     * Tests subscribing and unsubscribing from the event.
+     */
+
+    @Test
+    public void testSubscribeUnsubscribe()
+    {
+        ClassWithEvent aClassWithEvent = new ClassWithEvent();
+    
+        // Subscribe for the event.
+        aClassWithEvent.nameUpdated().subscribe(anEventHandler);
+        
+        // Use the class with the event, so it invokes the event.
+        aClassWithEvent.SetName("Mr. Smith");
+        
+        // Check the notified event.
+        assertEquals("Mr. Smith", myUpdatedName);
+        
+        // Unsubscribe from the event.
+        aClassWithEvent.nameUpdated().unsubscribe(anEventHandler);
+        
+        myUpdatedName = null;
+        
+        // Use the class with the event, so it invokes the event.
+        aClassWithEvent.SetName("bla bla");
+        
+        // Check the notified event.
+        // Note: Nothing should be received because we are unsubscribed.
+        assertNull(myUpdatedName);
+    }
+
+    // Event handler.
+    private void onNameUpdated(Object sender, String event)
+    {
+        myUpdatedName = event;
+    }
+    
+    
+    // Inner class providing the event handler.
+    private IMethod2<Object, String> anEventHandler = new IMethod2<Object, String>()
+    {
+        @Override
+        public void invoke(Object t1, String t2)
+        {
+            onNameUpdated(t1, t2);
+        }
+    };
+    
+    
+    private String myUpdatedName;
+}
