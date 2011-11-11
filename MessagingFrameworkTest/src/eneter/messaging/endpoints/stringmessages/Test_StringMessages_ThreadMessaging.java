800c483e192782c9549258e39865af318c0202e9
Ondrej Uzovic <ondrej.uzovic@gmail.com>
2011-11-11 22:14:39 +0000

    added additional tests for StringMessageSender and StringMessageReceiver - ThreadMessaging and ThreadPoolMessaging.

---
+++ MessagingFrameworkTest/src/eneter/messaging/endpoints/stringmessages/Test_StringMessages_ThreadMessaging.java
@@ -0,0 +1,18 @@
+package eneter.messaging.endpoints.stringmessages;
+
+import org.junit.*;
+
+import eneter.messaging.messagingsystems.messagingsystembase.*;
+import eneter.messaging.messagingsystems.threadmessagingsystem.ThreadMessagingSystemFactory;
+
+public class Test_StringMessages_ThreadMessaging extends StringMessagesBaseTester
+{
+    @Before
+    public void Setup()
+    {
+        IMessagingSystemFactory aMessagingSystem = new ThreadMessagingSystemFactory();
+        String aChannelId = "Channel1";
+
+        setup(aMessagingSystem, aChannelId);
+    }
+}
