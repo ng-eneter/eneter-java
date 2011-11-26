8f3c50d7c67638999c79fe0cd39811c3102ac3c0
Ondrej Uzovic <ondrej.uzovic@gmail.com>
2011-11-26 22:21:33 +0000

    unit-tests for DuplexChannelWrapper/Unwrapper.

---
+++ MessagingFrameworkTest/src/eneter/messaging/nodes/channelwrapper/Tester_DuplexChannelWrapper.java
@@ -0,0 +1,145 @@
+package eneter.messaging.nodes.channelwrapper;
+
+import static org.junit.Assert.*;
+
+import org.junit.*;
+
+import eneter.messaging.endpoints.stringmessages.*;
+import eneter.messaging.messagingsystems.messagingsystembase.*;
+import eneter.messaging.messagingsystems.synchronousmessagingsystem.*;
+import eneter.net.system.IMethod2;
+
+public class Tester_DuplexChannelWrapper
+{
+    @Before
+    public void setup()
+    {
+        myGlobalMessaging = new SynchronousMessagingSystemFactory();
+        myDuplexGlobalOutputChannel = myGlobalMessaging.createDuplexOutputChannel("MainChannel");
+        myDuplexGlobalInputChannel = myGlobalMessaging.createDuplexInputChannel("MainChannel");
+
+        myLocalMessaging1 = new SynchronousMessagingSystemFactory();
+        myLocalMessaging2 = new SynchronousMessagingSystemFactory();
+        
+        IChannelWrapperFactory aFactory = new ChannelWrapperFactory();
+        myDuplexChannelWrapper = aFactory.createDuplexChannelWrapper();
+        myDuplexChannelUnwrapper = aFactory.createDuplexChannelUnwrapper(myLocalMessaging2);
+    }
+    
+    @Test
+    public void WrapUnwrapMessage() throws Exception
+    {
+        // Wrapped/unwrapped channels
+        String aChannel1Id = "Channel1Id";
+        String aChannel2Id = "Channel2Id";
+
+        IDuplexStringMessagesFactory aStringMessagesFactory = new DuplexStringMessagesFactory();
+        
+        final IDuplexStringMessageReceiver aStringMessageReceiver1 = aStringMessagesFactory.createDuplexStringMessageReceiver();
+        final IDuplexStringMessageReceiver aStringMessageReceiver2 = aStringMessagesFactory.createDuplexStringMessageReceiver();
+
+        final IDuplexStringMessageSender aStringMessageSender1 = aStringMessagesFactory.createDuplexStringMessageSender();
+        final IDuplexStringMessageSender aStringMessageSender2 = aStringMessagesFactory.createDuplexStringMessageSender();
+
+        
+        // Sender side
+        IDuplexInputChannel anInputChannel11 = myLocalMessaging1.createDuplexInputChannel(aChannel1Id);
+        IDuplexOutputChannel anOutputChannel11 = myLocalMessaging1.createDuplexOutputChannel(aChannel1Id);
+        
+        IDuplexInputChannel anInputChannel12 = myLocalMessaging1.createDuplexInputChannel(aChannel2Id);
+        IDuplexOutputChannel anOutputChannel12 = myLocalMessaging1.createDuplexOutputChannel(aChannel2Id);
+        
+        myDuplexChannelWrapper.attachDuplexInputChannel(anInputChannel11);
+        aStringMessageSender1.attachDuplexOutputChannel(anOutputChannel11);
+        
+        myDuplexChannelWrapper.attachDuplexInputChannel(anInputChannel12);
+        aStringMessageSender2.attachDuplexOutputChannel(anOutputChannel12);
+        
+        // Receiver side
+        IDuplexInputChannel anInputChannel21 = myLocalMessaging2.createDuplexInputChannel(aChannel1Id);
+        IDuplexInputChannel anInputChannel22 = myLocalMessaging2.createDuplexInputChannel(aChannel2Id);
+        aStringMessageReceiver1.attachDuplexInputChannel(anInputChannel21);
+        aStringMessageReceiver2.attachDuplexInputChannel(anInputChannel22);
+        
+        
+        // Connect wrapper and unwrapper to global channels.
+        myDuplexChannelUnwrapper.attachDuplexInputChannel(myDuplexGlobalInputChannel);
+        myDuplexChannelWrapper.attachDuplexOutputChannel(myDuplexGlobalOutputChannel);
+
+
+        final StringRequestReceivedEventArgs[] aReceivedMessage1 = {null};
+        aStringMessageReceiver1.requestReceived().subscribe(new IMethod2<Object, StringRequestReceivedEventArgs>()
+        {
+            @Override
+            public void invoke(Object x, StringRequestReceivedEventArgs y)
+                    throws Exception
+            {
+                aReceivedMessage1[0] = y;
+                aStringMessageReceiver1.sendResponseMessage(y.getResponseReceiverId(), "Response1");
+            }
+        });
+        
+
+        final StringRequestReceivedEventArgs[] aReceivedMessage2 = {null};
+        aStringMessageReceiver2.requestReceived().subscribe(new IMethod2<Object, StringRequestReceivedEventArgs>()
+        {
+            @Override
+            public void invoke(Object x, StringRequestReceivedEventArgs y)
+                    throws Exception
+            {
+                aReceivedMessage2[0] = y;
+                aStringMessageReceiver2.sendResponseMessage(y.getResponseReceiverId(), "Response2");
+            }
+        });
+
+        final StringResponseReceivedEventArgs[] aReceivedResponse1 = {null};
+        aStringMessageSender1.responseReceived().subscribe(new IMethod2<Object, StringResponseReceivedEventArgs>()
+        {
+            @Override
+            public void invoke(Object x, StringResponseReceivedEventArgs y)
+                    throws Exception
+            {
+                aReceivedResponse1[0] = y;
+            }
+        });
+
+        final StringResponseReceivedEventArgs[] aReceivedResponse2 = {null};
+        aStringMessageSender2.responseReceived().subscribe(new IMethod2<Object, StringResponseReceivedEventArgs>()
+        {
+            @Override
+            public void invoke(Object x, StringResponseReceivedEventArgs y)
+                    throws Exception
+            {
+                aReceivedResponse2[0] = y;
+            }
+        });
+
+        aStringMessageSender1.sendMessage("Message1");
+
+        assertEquals("Message1", aReceivedMessage1[0].getRequestMessage());//, "Message receiver 1 received incorrect message.");
+        assertEquals("Response1", aReceivedResponse1[0].getResponseMessage());//, "Response receiver 1 received incorrect message.");
+        assertNull(aReceivedMessage2[0]);//, "Message receiver 2 should not receive a message.");
+        assertNull(aReceivedResponse2[0]);//, "Response receiver 2 should not receive a message.");
+
+
+        aReceivedMessage1[0] = null;
+        aReceivedResponse1[0] = null;
+
+        aStringMessageSender2.sendMessage("Message2");
+
+        assertEquals("Message2", aReceivedMessage2[0].getRequestMessage());//, "Message receiver 2 received incorrect message.");
+        assertEquals("Response2", aReceivedResponse2[0].getResponseMessage());//, "Response receiver 2 received incorrect message.");
+        assertNull(aReceivedMessage1[0]);//, "Message receiver 1 should not receive a message.");
+        assertNull(aReceivedResponse1[0]);//, "Response receiver 1 should not receive a message.");
+    }
+
+    private IMessagingSystemFactory myGlobalMessaging;
+    private IMessagingSystemFactory myLocalMessaging1;
+    private IMessagingSystemFactory myLocalMessaging2;
+    
+    private IDuplexChannelWrapper myDuplexChannelWrapper;
+    private IDuplexChannelUnwrapper myDuplexChannelUnwrapper;
+
+    private IDuplexOutputChannel myDuplexGlobalOutputChannel;
+    private IDuplexInputChannel myDuplexGlobalInputChannel;
+}
