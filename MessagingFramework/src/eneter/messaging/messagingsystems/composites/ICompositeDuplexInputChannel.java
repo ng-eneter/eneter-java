bb2a1f16a28e169e3f482b9ad26f85646616c4a7
Ondrej Uzovic <ondrej.uzovic@gmail.com>
2012-02-03 21:32:53 +0000

    docs update

--- MessagingFramework/src/eneter/messaging/messagingsystems/composites/ICompositeDuplexInputChannel.java
+++ MessagingFramework/src/eneter/messaging/messagingsystems/composites/ICompositeDuplexInputChannel.java
@@ -10,7 +10,17 @@ package eneter.messaging.messagingsystems.composites;
 
 import eneter.messaging.messagingsystems.messagingsystembase.IDuplexInputChannel;
 
+/**
+ * The interface declares the composite duplex input channel.
+ * The composite duplex input channel is the duplex input channel that uses underlying duplex input channel
+ * to receive messages and send responses messages.
+ *
+ */
 public interface ICompositeDuplexInputChannel extends IDuplexInputChannel
 {
+    /**
+     * Returns the underlying duplex input channel.
+     * @return
+     */
     IDuplexInputChannel getUnderlyingDuplexInputChannel();
 }
--- MessagingFramework/src/eneter/messaging/messagingsystems/composites/ICompositeOutputChannel.java
+++ MessagingFramework/src/eneter/messaging/messagingsystems/composites/ICompositeOutputChannel.java
@@ -10,7 +10,17 @@ package eneter.messaging.messagingsystems.composites;
 
 import eneter.messaging.messagingsystems.messagingsystembase.IOutputChannel;
 
+/**
+ * The interface declares the composit output channel.
+ * The composit output channel is the output channel that uses underlying output channel
+ * to send messages. 
+ *
+ */
 public interface ICompositeOutputChannel extends IOutputChannel
 {
+    /**
+     * Returns the underlying output channel.
+     * @return
+     */
     IOutputChannel getUnderlyingOutputChannel();
 }
