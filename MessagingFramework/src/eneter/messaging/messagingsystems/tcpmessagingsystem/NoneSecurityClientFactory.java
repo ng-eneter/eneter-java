60c5036691020a9f443e93dcd6fd555ad342c967
Ondrej Uzovic <ondrej.uzovic@gmail.com>
2012-02-24 11:09:44 +0000

    possibility to setup the connection timeout added

--- MessagingFramework/src/eneter/messaging/messagingsystems/tcpmessagingsystem/NoneSecurityClientFactory.java
+++ MessagingFramework/src/eneter/messaging/messagingsystems/tcpmessagingsystem/NoneSecurityClientFactory.java
@@ -21,6 +21,25 @@ import eneter.messaging.diagnostic.EneterTrace;
 public class NoneSecurityClientFactory implements IClientSecurityFactory
 {
     /**
+     * Constructs the factory that creates a normal socket on the client side.
+     * The connection timeout is set to 3000 milliseconds.
+     */
+    public NoneSecurityClientFactory()
+    {
+        this(3000);
+    }
+    
+    /**
+     * Constructs the factory that creates a normal socket on the client side.
+     * 
+     * @param connectionTimeout timeout in milliseconds for opening connection 
+     */
+    public NoneSecurityClientFactory(int connectionTimeout)
+    {
+        myConnectionTimeout = connectionTimeout;
+    }
+    
+    /**
      * Creates non-secured client socket.
      */
     @Override
@@ -32,7 +51,7 @@ public class NoneSecurityClientFactory implements IClientSecurityFactory
             Socket aClientSocket = new Socket();
             
             // Connect with the timeout 3 seconds.
-            aClientSocket.connect(socketAddress, 3000);
+            aClientSocket.connect(socketAddress, myConnectionTimeout);
             aClientSocket.setTcpNoDelay(true);
             return aClientSocket;
         }
@@ -41,4 +60,6 @@ public class NoneSecurityClientFactory implements IClientSecurityFactory
             EneterTrace.leaving(aTrace);
         }
     }
+    
+    private int myConnectionTimeout;
 }
