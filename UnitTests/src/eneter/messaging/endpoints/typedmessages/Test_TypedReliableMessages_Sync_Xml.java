96b1d11289112300b0346725117520fc9245f01d
Ondrej Uzovic <ondrej.uzovic@gmail.com>
2012-12-28 16:41:24 +0000

    ReliableMessages, unit-tests added.

---
+++ UnitTests/src/eneter/messaging/endpoints/typedmessages/Test_TypedReliableMessages_Sync_Xml.java
@@ -0,0 +1,21 @@
+package eneter.messaging.endpoints.typedmessages;
+
+import org.junit.Before;
+
+import eneter.messaging.dataprocessing.serializing.*;
+import eneter.messaging.messagingsystems.messagingsystembase.IMessagingSystemFactory;
+import eneter.messaging.messagingsystems.synchronousmessagingsystem.SynchronousMessagingSystemFactory;
+
+public class Test_TypedReliableMessages_Sync_Xml extends TypedReliableMessagesBaseTester
+{
+    @Before
+    public void setup() throws Exception
+    {
+        IMessagingSystemFactory aMessagingSystem = new SynchronousMessagingSystemFactory();
+
+        String aChannelId = "Channel1";
+        ISerializer aSerializer = new XmlStringSerializer();
+
+        Setup(aMessagingSystem, aChannelId, aSerializer);
+    }
+}
