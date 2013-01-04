77aab541ffeefa5fe304044ab53a75b388782cdd
Ondrej Uzovic <ondrej.uzovic@gmail.com>
2013-01-04 10:55:14 +0000

    AndroidUsbCableMessagingFactory added.

---
+++ MessagingFramework/src/eneter/messaging/messagingsystems/androidusbcablemessagingsystem/AndroidUsbCableMessagingFactory.java
@@ -0,0 +1,63 @@
+package eneter.messaging.messagingsystems.androidusbcablemessagingsystem;
+
+import eneter.messaging.diagnostic.EneterTrace;
+import eneter.messaging.messagingsystems.connectionprotocols.IProtocolFormatter;
+import eneter.messaging.messagingsystems.messagingsystembase.*;
+
+public class AndroidUsbCableMessagingFactory implements IMessagingSystemFactory
+{
+
+    @Override
+    public IOutputChannel createOutputChannel(String channelId)
+            throws Exception
+    {
+        throw new UnsupportedOperationException("One-way output channel is not supported for Android USB cable messaging.");
+    }
+
+    @Override
+    public IInputChannel createInputChannel(String channelId) throws Exception
+    {
+        throw new UnsupportedOperationException("One-way input channel is not supported for Android USB cable messaging.");
+    }
+
+    @Override
+    public IDuplexOutputChannel createDuplexOutputChannel(String channelId)
+            throws Exception
+    {
+        EneterTrace aTrace = EneterTrace.entering();
+        try
+        {
+            return new AndroidUsbDuplexOutputChannel(Integer.parseInt(channelId), null, myAdbHostPort, myProtocolFormatter);
+        }
+        finally
+        {
+            EneterTrace.leaving(aTrace);
+        }
+    }
+
+    @Override
+    public IDuplexOutputChannel createDuplexOutputChannel(String channelId,
+            String responseReceiverId) throws Exception
+    {
+        EneterTrace aTrace = EneterTrace.entering();
+        try
+        {
+            return new AndroidUsbDuplexOutputChannel(Integer.parseInt(channelId), responseReceiverId, myAdbHostPort, myProtocolFormatter);
+        }
+        finally
+        {
+            EneterTrace.leaving(aTrace);
+        }
+    }
+
+    @Override
+    public IDuplexInputChannel createDuplexInputChannel(String channelId)
+            throws Exception
+    {
+        throw new UnsupportedOperationException("Duplex input channel is not supported for Android USB cable messaging.");
+    }
+
+    
+    private int myAdbHostPort;
+    private IProtocolFormatter<byte[]> myProtocolFormatter;
+}
