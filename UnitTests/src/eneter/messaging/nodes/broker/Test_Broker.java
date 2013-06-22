699f555819796327cd56cbd75cc8fff6ba667b6c
Ondrej Uzovic <ondrej.uzovic@gmail.com>
2013-06-22 11:57:34 +0000

    unit-tests added for broker

--- UnitTests/src/eneter/messaging/nodes/broker/Test_Broker.java
+++ UnitTests/src/eneter/messaging/nodes/broker/Test_Broker.java
@@ -3,12 +3,16 @@ package eneter.messaging.nodes.broker;
 import static org.junit.Assert.*;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 import org.junit.Test;
 
+import eneter.messaging.dataprocessing.serializing.JavaBinarySerializer;
 import eneter.messaging.messagingsystems.messagingsystembase.*;
 import eneter.messaging.messagingsystems.synchronousmessagingsystem.SynchronousMessagingSystemFactory;
+import eneter.messaging.messagingsystems.tcpmessagingsystem.TcpMessagingSystemFactory;
 import eneter.net.system.EventHandler;
+import eneter.net.system.threading.internal.AutoResetEvent;
 
 public class Test_Broker
 {
@@ -270,5 +274,166 @@ public class Test_Broker
         // Subscribe.
         String[] aTypes = {"*", null};
         aSubscriber.subscribe(aTypes);
+    }
+    
+    @Test
+    public void DoNotNotifyPublisher() throws Exception
+    {
+        // Create channels
+        IMessagingSystemFactory aMessagingSystem = new SynchronousMessagingSystemFactory();
+
+        IDuplexInputChannel aBrokerInputChannel = aMessagingSystem.createDuplexInputChannel("BrokerChannel");
+        IDuplexOutputChannel aClient1OutputChannel = aMessagingSystem.createDuplexOutputChannel("BrokerChannel");
+        IDuplexOutputChannel aClient2OutputChannel = aMessagingSystem.createDuplexOutputChannel("BrokerChannel");
+
+        // Specify in the factory that the publisher shall not be notified from its own published events.
+        IDuplexBrokerFactory aBrokerFactory = new DuplexBrokerFactory(false);
+
+        IDuplexBroker aBroker = aBrokerFactory.createBroker();
+        aBroker.attachDuplexInputChannel(aBrokerInputChannel);
+
+        IDuplexBrokerClient aClient1 = aBrokerFactory.createBrokerClient();
+        final ArrayList<BrokerMessageReceivedEventArgs> aClient1ReceivedMessage = new ArrayList<BrokerMessageReceivedEventArgs>();
+        aClient1.brokerMessageReceived().subscribe(new EventHandler<BrokerMessageReceivedEventArgs>()
+        {
+            @Override
+            public void onEvent(Object x, BrokerMessageReceivedEventArgs y)
+            {
+                aClient1ReceivedMessage.add(y);
+            }
+        });
+        aClient1.attachDuplexOutputChannel(aClient1OutputChannel);
+
+        IDuplexBrokerClient aClient2 = aBrokerFactory.createBrokerClient();
+        final ArrayList<BrokerMessageReceivedEventArgs> aClient2ReceivedMessage = new ArrayList<BrokerMessageReceivedEventArgs>();
+        aClient2.brokerMessageReceived().subscribe(new EventHandler<BrokerMessageReceivedEventArgs>()
+        {
+            @Override
+            public void onEvent(Object x, BrokerMessageReceivedEventArgs y)
+            {
+                aClient2ReceivedMessage.add(y);
+            }
+        });
+        aClient2.attachDuplexOutputChannel(aClient2OutputChannel);
+
+
+        aClient1.subscribe("TypeA");
+        aClient2.subscribe("TypeA");
+
+
+        // Notify the message.
+        aClient2.sendMessage("TypeA", "Message A");
+
+        // Client 2 should not get the notification.
+        assertEquals(1, aClient1ReceivedMessage.size());
+        assertEquals(0, aClient2ReceivedMessage.size());
+        assertEquals("TypeA", aClient1ReceivedMessage.get(0).getMessageTypeId());
+        assertEquals("Message A", (String)aClient1ReceivedMessage.get(0).getMessage());
+    }
+    
+    @Test
+    public void Notify_50000() throws Exception
+    {
+        // Create channels
+        IMessagingSystemFactory aMessagingSystem = new SynchronousMessagingSystemFactory();
+
+        IDuplexInputChannel aBrokerInputChannel = aMessagingSystem.createDuplexInputChannel("BrokerChannel");
+        IDuplexOutputChannel aClient1OutputChannel = aMessagingSystem.createDuplexOutputChannel("BrokerChannel");
+        IDuplexOutputChannel aClient2OutputChannel = aMessagingSystem.createDuplexOutputChannel("BrokerChannel");
+
+        // Specify in the factory that the publisher shall not be notified from its own published events.
+        IDuplexBrokerFactory aBrokerFactory = new DuplexBrokerFactory(false, new JavaBinarySerializer());
+
+        IDuplexBroker aBroker = aBrokerFactory.createBroker();
+        aBroker.attachDuplexInputChannel(aBrokerInputChannel);
+
+        IDuplexBrokerClient aClient1 = aBrokerFactory.createBrokerClient();
+        final int[] aCount = { 0 };
+        aClient1.brokerMessageReceived().subscribe(new EventHandler<BrokerMessageReceivedEventArgs>()
+        {
+            @Override
+            public void onEvent(Object sender, BrokerMessageReceivedEventArgs e)
+            {
+                ++aCount[0];
+            }
+        });
+        aClient1.attachDuplexOutputChannel(aClient1OutputChannel);
+
+        IDuplexBrokerClient aClient2 = aBrokerFactory.createBrokerClient();
+        aClient2.attachDuplexOutputChannel(aClient2OutputChannel);
+
+        aClient1.subscribe("TypeA");
+
+        for (int i = 0; i < 50000; ++i)
+        {
+            // Notify the message.
+            aClient2.sendMessage("TypeA", "Message A");
+        }
+
+        // Client 2 should not get the notification.
+        assertEquals(50000, aCount[0]);
+    }
+    
+    @Test
+    public void Notify_50000_TCP() throws Exception
+    {
+        Random aRandomPort = new Random();
+        int aPort = 7000 + aRandomPort.nextInt(1000);
+        
+        // Create channels
+        IMessagingSystemFactory aMessagingSystem = new TcpMessagingSystemFactory();
+
+        IDuplexInputChannel aBrokerInputChannel = aMessagingSystem.createDuplexInputChannel("tcp://127.0.0.1:" + aPort + "/");
+        IDuplexOutputChannel aClient1OutputChannel = aMessagingSystem.createDuplexOutputChannel("tcp://127.0.0.1:" + aPort + "/");
+        IDuplexOutputChannel aClient2OutputChannel = aMessagingSystem.createDuplexOutputChannel("tcp://127.0.0.1:" + aPort + "/");
+
+        // Specify in the factory that the publisher shall not be notified from its own published events.
+        IDuplexBrokerFactory aBrokerFactory = new DuplexBrokerFactory(false, new JavaBinarySerializer());
+
+        IDuplexBroker aBroker = aBrokerFactory.createBroker();
+        aBroker.attachDuplexInputChannel(aBrokerInputChannel);
+
+        IDuplexBrokerClient aClient1 = aBrokerFactory.createBrokerClient();
+        final int[] aCount = { 0 };
+        final AutoResetEvent aCompletedEvent = new AutoResetEvent(false);
+        aClient1.brokerMessageReceived().subscribe(new EventHandler<BrokerMessageReceivedEventArgs>()
+        {
+            
+            @Override
+            public void onEvent(Object sender, BrokerMessageReceivedEventArgs e)
+            {
+                ++aCount[0];
+                if (aCount[0] == 50000)
+                {
+                    aCompletedEvent.set();
+                }
+            }
+        });
+        aClient1.attachDuplexOutputChannel(aClient1OutputChannel);
+
+        IDuplexBrokerClient aClient2 = aBrokerFactory.createBrokerClient();
+        aClient2.attachDuplexOutputChannel(aClient2OutputChannel);
+
+        try
+        {
+            aClient1.subscribe("TypeA");
+
+            for (int i = 0; i < 50000; ++i)
+            {
+                // Notify the message.
+                aClient2.sendMessage("TypeA", "Message A");
+            }
+
+            aCompletedEvent.waitOne();
+
+            // Client 2 should not get the notification.
+            assertEquals(50000, aCount[0]);
+        }
+        finally
+        {
+            aClient1.detachDuplexOutputChannel();
+            aClient2.detachDuplexOutputChannel();
+            aBroker.detachDuplexInputChannel();
+        }
     }
 }
