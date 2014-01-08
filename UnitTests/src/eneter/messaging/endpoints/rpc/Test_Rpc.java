bb41803c79d6b6403a53a91376c69eafd61736ff
Ondrej Uzovic <ondrej.uzovic@gmail.com>
2014-01-08 22:28:08 +0000

    Rpc test updated.

--- UnitTests/src/eneter/messaging/endpoints/rpc/Test_Rpc.java
+++ UnitTests/src/eneter/messaging/endpoints/rpc/Test_Rpc.java
@@ -1,5 +1,7 @@
 package eneter.messaging.endpoints.rpc;
 
+import java.util.ArrayList;
+
 import org.junit.*;
 
 import eneter.net.system.*;
@@ -12,14 +14,18 @@ public class Test_Rpc
         Event<EventArgs> nonGenericEvent();
         Event<String> genericEvent();
         
-        int Calculate();
+        int Calculate(int a, int b);
+        
+        void DoIt();
     }
     
     @Test
-    public void Test1()
+    public void Test1() throws Exception
     {
         RpcFactory aFactory = new RpcFactory();
         
         IRpcClient<IFooService> aClient = aFactory.createClient(IFooService.class);
+        
+        aClient.callRemoteMethod("Calculate", new Object[] {1, 2});
     }
 }
