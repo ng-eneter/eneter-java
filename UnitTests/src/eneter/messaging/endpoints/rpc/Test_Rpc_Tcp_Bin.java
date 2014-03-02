c96c75ab8a58626e7a226eeeebb9f70bb18a5ec7
Ondrej Uzovic <ondrej.uzovic@gmail.com>
2014-03-02 15:20:49 +0000

    Cosmetic change in RPC u-test.

--- UnitTests/src/eneter/messaging/endpoints/rpc/Test_Rpc_Tcp_Bin.java
+++ UnitTests/src/eneter/messaging/endpoints/rpc/Test_Rpc_Tcp_Bin.java
@@ -1,15 +1,23 @@
 package eneter.messaging.endpoints.rpc;
 
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
+
 import org.junit.Before;
 
 import eneter.messaging.dataprocessing.serializing.JavaBinarySerializer;
+import eneter.messaging.diagnostic.EneterTrace;
+import eneter.messaging.diagnostic.EneterTrace.EDetailLevel;
 import eneter.messaging.messagingsystems.tcpmessagingsystem.TcpMessagingSystemFactory;
 
 public class Test_Rpc_Tcp_Bin extends RpcBaseTester
 {
     @Before
-    public void setup()
+    public void setup() throws FileNotFoundException
     {
+        //EneterTrace.setTraceLog(new PrintStream("D:\\Trace.txt"));
+        //EneterTrace.setDetailLevel(EDetailLevel.Debug);
+        
         mySerializer = new JavaBinarySerializer();
         myChannelId = "tcp://127.0.0.1:8095/";
         myMessaging = new TcpMessagingSystemFactory();
