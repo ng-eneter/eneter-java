572cbbb772d062f0340e49d879f14ac10e42551e
Ondrej Uzovic <ondrej.uzovic@gmail.com>
2015-11-20 18:25:43 +0000

    GetSerializerCallback documentation updated.

--- MessagingFramework/src/eneter/messaging/dataprocessing/serializing/GetSerializerCallback.java
+++ MessagingFramework/src/eneter/messaging/dataprocessing/serializing/GetSerializerCallback.java
@@ -11,7 +11,7 @@ package eneter.messaging.dataprocessing.serializing;
 /**
  * Callback used by communication components to get serializer for a specific client connection.
  * If the callback is specified then the service communication component calls it whenever it needs to serialize/deserialize
- * the communicatation with the client. The purpose of this callback is to allow to use for each client a different serializer.
+ * the communication with the client. The purpose of this callback is to allow to use for each client a different serializer.
  * E.g. if the serialized message shall be encrypted by a client specific password or a key.
  */
 public interface GetSerializerCallback
